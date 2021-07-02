1f1d1efea291b86ef9704c203484ce6d9ced9e1b
lordjette <lordjette.leigh@gmail.com>
2021-07-02 22:00:56 +0800

    gggg

--- NOTES/notes_select_list_of_import_all.py
+++
@@ -1,8 +0,0 @@
->>> vars_before_import = set(dir())
->>> from json import *
->>> set(dir()) - vars_before_import
-set(['load', 'JSONEncoder', 'dump', 'vars_before_import', 'JSONDecoder', 'dumps', 'loads'])
-
-
-
-
